88b2acaf2b8f3a4637d679befbf31c3a98b99dd7
Eric Park <me@ericswpark.com>
2024-10-27 12:08:31 -0400

    Add initial layout for home view with housing options

--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+
+from core.models import Housing
 
 
-class HomeView(TemplateView):
+class HomeView(ListView):
     template_name = "home.html"
-
+    model = Housing
+    context_object_name = "housing_options"
